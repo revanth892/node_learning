029c9723f9b199e05d7e4f6ad3104b4ee67bf6a3
revanth892 <revanth9347569169@gmail.com>
2023-10-19 14:24:07 +0530

    Understanding basic request and response

--- NODE_JS_MODULE_3/app.js
+++ NODE_JS_MODULE_3/app.js
@@ -1,7 +1,16 @@
 const http=require('http');
 
 const server =http.createServer((req,res)=>{
-    console.log(req);
+    // console.log(req);
+    // process.exit(); to unregister the event but we dont use this as server should be running
+
+    console.log(req.url,req.method,req.headers)
+    res.setHeader('Content-Type','text/html'); //if we remove this line its still working ,res.headers in network is not showing this header
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server</h1></body>')
+    res.write('</html>');
+    res.end()
 })
 
 server.listen(3030);
